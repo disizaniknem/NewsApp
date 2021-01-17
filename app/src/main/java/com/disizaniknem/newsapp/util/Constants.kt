6aa28553cf9a73e0a13d49053e37e9316bb1239d
vdelll <comptedemo80@gmail.com>
2021-01-17 20:24:46 +0100

    Pagination

--- app/src/main/java/com/disizaniknem/newsapp/util/Constants.kt
+++ app/src/main/java/com/disizaniknem/newsapp/util/Constants.kt
@@ -6,5 +6,6 @@ class Constants {
         const val API_KEY = "0717d2ea46c84f969aed1f1de9c19b28"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 }
